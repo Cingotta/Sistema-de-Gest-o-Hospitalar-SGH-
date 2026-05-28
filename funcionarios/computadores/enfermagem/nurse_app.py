b459043d3d9bbe04625d07b6fd310404f10f6023
Enzo Prata Cingotta <enzopratacingotta@gmail.com>
2026-05-28 09:33:03 -0300

    funçoes basico enfermeiras

--- funcionarios/computadores/enfermagem/nurse_app.py
+++ funcionarios/computadores/enfermagem/nurse_app.py
@@ -1,3 +1,5 @@
+#atualização rápida de pacientes
+
 import sqlite3
 import os
 
@@ -5,3 +7,23 @@ _DIR = os.path.dirname(os.path.abspath(__file__))
 CACHE_DB = os.path.join(_DIR, '..', '..', '..', 'clientes', 'recepcao', 'cache.db')
 AUTH_DB = os.path.join(_DIR, '..', '..', '..', 'funcionarios', 'principal', 'auth.db')
 
+
+def criar_tabela_patient():
+    conexao = sqlite3.connect(AUTH_DB)
+    cursor = conexao.cursor()
+
+    cursor.execute("PRAGMA foreign_keys = ON")
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS patient_db (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            idade INTEGER NOT NULL,
+            doenca TEXT,
+            tipo_doenca TEXT
+        )
+        """
+    )
+    conexao.commit()
+    conexao.close()
